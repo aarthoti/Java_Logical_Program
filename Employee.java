3ef0145719ce12dc58a8319b44ab644ea80f5a8f
aarthoti <165478144+aarthoti@users.noreply.github.com>
2024-11-06 16:43:55 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,13 @@
+import java.lang.System;
+import java.lang.String;
+class Employee{
+public static void main(String arg[])
+{
+int eno=101;
+String ename="karun";
+Float esal=54F;
+System.out.println("THE GIVEN EMPLOYEE NUMBER:"+eno);
+System.out.println("THE GIVEN EMPLOYEE NAME :"+ename);
+System.out.println("THE GIVEN EMPLOYEE SLARY:"+esal);
+}
+}
---
+++ First.java
@@ -0,0 +1,8 @@
+import java.lang.System;
+import java.lang.String;
+class First{
+public static void main(String arg[])
+{
+System.out.print("Welcome to first java");
+}
+}
